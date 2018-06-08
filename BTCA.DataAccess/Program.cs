eb54bbb33f707433c586af61741ccbc77b52d49d
Brian Thomas <brian.thomas@btechnical-consulting.com>
2018-06-08 14:09:35 -0500

    Added log directories and DataAccess project

---
+++ BTCA.DataAccess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BTCA.DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
