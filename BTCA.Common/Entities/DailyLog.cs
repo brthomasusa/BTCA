6d1056f40517467a05982014b3a9c19c090e0e12
Brian Thomas <brian.thomas@btechnical-consulting.com>
2018-06-17 12:56:29 -0500

    Finished

--- BTCA.Common/Entities/DailyLog.cs
+++ BTCA.Common/Entities/DailyLog.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -10,13 +11,33 @@ namespace BTCA.Common.Entities
         [Key]
         public virtual int LogID { get; set; }
 
+        [Required, Display(Name = "Log Date"), DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)]
+        public virtual DateTime LogDate { get; set; }
+
+        [Display(Name = "Beginning Mileage")]
+        public virtual int BeginningMileage { get; set; }
+
+        [Display(Name = "Ending Mileage")]
+        public virtual int EndingMileage { get; set; }
+
+        [Required, MaxLength(25), Display(Name = "Truck Number")]
+        public virtual string TruckNumber { get; set; }
+
+        [MaxLength(25), Display(Name = "Trailer Number")]
+        public virtual string TrailerNumber { get; set; }
 
         [MaxLength(4000)]
-        public virtual string Notes { get; set; }
+        public virtual string Notes { get; set; }         
 
-        public virtual int CompanyId { get; set; }
+        [Required, Display(Name = "Driver ID")]  // AppUser.Id
+        public virtual int DriverID { get; set; }
 
-        [ForeignKey(nameof(CompanyId))]
-        public virtual Company Company { get; set; }         
+        [ForeignKey(nameof(DriverID))]
+
+        public virtual AppUser Driver { get; set; } 
+
+        [InverseProperty(nameof(DailyLogDetail.DailyLog))]      
+        public virtual IList<DailyLogDetail> DailyLogDetails { get; set; } = new List<DailyLogDetail>();              
     }
 }
