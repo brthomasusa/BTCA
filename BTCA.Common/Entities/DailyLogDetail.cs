8fadd58d67054516b203d37309355c495d588cdd
Brian Thomas <brian.thomas@btechnical-consulting.com>
2018-06-17 12:56:54 -0500

    Finished

--- BTCA.Common/Entities/DailyLogDetail.cs
+++ BTCA.Common/Entities/DailyLogDetail.cs
@@ -8,5 +8,40 @@ namespace BTCA.Common.Entities
 {
     public class DailyLogDetail : BaseEntity
     {
+        [Key]
+        public virtual int LogDetailID { get; set; }
+
+        [Required, DataType(DataType.DateTime), Display(Name = "Start Time")]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy HH:mm}", ApplyFormatInEditMode = true)]
+        public virtual DateTime StartTime { get; set; }
+
+
+
+
+        [Required]
+        public virtual int LogID { get; set; }
+
+        [ForeignKey(nameof(LogID))]
+
+        public virtual DailyLog DailyLog { get; set; } 
+
+        [Required]
+        public virtual int DutyStatusID { get; set; } 
+
+        [ForeignKey(nameof(DutyStatusID))]
+
+        public virtual DutyStatus DutyStatus { get; set; }
+
+        public virtual int StateProvinceId { get; set; }
+
+        [ForeignKey(nameof(StateProvinceId))]
+
+        public virtual StateProvinceCode StateProvinceCode { get; set; }
+
+        public virtual int DutyStatusActivityID { get; set; }
+
+        [ForeignKey(nameof(DutyStatusActivityID))] 
+
+        public virtual DutyStatusActivity DutyStatusActivity { get; set; }       
     }
 }
