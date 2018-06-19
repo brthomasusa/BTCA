02a214a8f5eca2d13ddfef3daef9710b43b85389
Brian Thomas <brian.thomas@btechnical-consulting.com>
2018-06-18 21:51:24 -0500

    Final refactoring before new branch

---
+++ BTCA.DomainLayer/Managers/Interface/ICompanyUserManager.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+using BTCA.Common.Entities;
+using BTCA.Common.BusinessObjects;
+
+namespace BTCA.DomainLayer.Managers.Interface
+{
+    public interface ICompanyUserManager
+    {
+        IEnumerable<CompanyUser> GetAll();
+        IEnumerable<CompanyUser> GetCompanyUsers(int companyId);
+        CompanyUser GetCompanyUser(int companyId, int userId);
+        CompanyUser GetCompanyUser(Expression<Func<AppUser, bool>> expression);         
+    }
+}
