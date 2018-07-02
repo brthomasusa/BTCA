c31253e4bbb05cddb62dbc0e631415143f4304de
Brian Thomas <brian.thomas@btechnical-consulting.com>
2018-07-02 13:37:54 -0500

    Requires EF Core 2.1'

--- BTCA.Common/BusinessObjects/DailyLogDetailModel.cs
+++ BTCA.Common/BusinessObjects/DailyLogDetailModel.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using BTCA.Common.Core;
 
 namespace BTCA.Common.BusinessObjects
