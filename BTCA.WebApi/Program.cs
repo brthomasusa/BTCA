399b279df997f833103dc7549121e4b03d297c2b
Brian Thomas <brian.thomas@btechnical-consulting.com>
2018-07-04 05:15:30 -0500

    Added boilerplate code to startup files

--- BTCA.WebApi/Program.cs
+++ BTCA.WebApi/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,19 +7,53 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace BTCA.WebApi
 {
     public class Program
     {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build(); 
+
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            // NLog: setup the logger first to catch all errors
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("NLog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Info("BTCAv1.WebApi is starting ...");
+                BuildWebHost(args).Run(); 
+            }
+            catch (Exception ex)
+            {
+                //NLog: catch setup errors
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                logger.Info("BTCAv1.WebApi is shutting down ...");
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
                 .UseStartup<Startup>()
+                .UseUrls("http://localhost:5001")
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                .UseNLog()  // NLog: setup NLog for Dependency injection                
                 .Build();
     }
 }
--- BTCA.WebApi/Startup.cs
+++ BTCA.WebApi/Startup.cs
@@ -1,32 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using IdentityServer4.AccessTokenValidation;
+using BTCA.DomainLayer.Managers.Interface;
+using BTCA.DomainLayer.Managers.Implementation;
+using BTCA.DataAccess.Core;
+using BTCA.DataAccess.EF;
 
 namespace BTCA.WebApi
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters();
+
+            // services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+            //     .AddIdentityServerAuthentication(options =>
+            //     {
+            //         options.Authority = "http://localhost:50011";
+            //         options.RequireHttpsMetadata = false;
+            //         options.ApiName = "scope.fullaccess";
+            //         options.ApiSecret = "Info99Gum";
+            //     });
+
+            services.AddDbContext<HOSContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("HOS")));
+
+            services.AddScoped<IRepository, Repository>();
+            services.AddScoped<ICompanyManager, CompanyManager>();
+            services.AddScoped<ICompanyAddressManager, CompanyAddressManager>();
+            services.AddScoped<IStateProvinceCodeManager, StateProvinceCodeManager>();
+            services.AddScoped<ICompanyUserManager, CompanyUserManager>();                
+            services.AddScoped<IDailyLogManager, DailyLogManager>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -34,6 +52,9 @@ namespace BTCA.WebApi
                 app.UseDeveloperExceptionPage();
             }
 
+
+            app.UseStatusCodePages();
+            //app.UseAuthentication();            
             app.UseMvc();
         }
     }
