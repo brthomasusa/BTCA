6bf728267cc59587439e0590cf088f8192fb6100
Brian Thomas <brian.thomas@btechnical-consulting.com>
2018-07-12 06:29:32 -0500

    Initial commit

---
+++ BTCA.WebApi/Controllers/CompanyAddressController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BTCA.DomainLayer.Managers.Interface;
+using BTCA.Common.Entities;
+using BTCA.Common.BusinessObjects;
+
+namespace BTCA.WebApi.Controllers
+{
+    public class CompanyAddressController : Controller
+    {
+    }
+}
